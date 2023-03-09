319977eadc5bed84c16874e404d772bd88becd51
Neha kashyap <32489573+NENNU@users.noreply.github.com>
2023-03-09 15:33:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import React , {useState,useEffect} from 'react'
+import React from 'react'
 import '../styles/App.css';
 import ChildComponent from './ChildComponent';
+const ShowSquare = ({value}) => {
+    return (<p id='output' value={value}>{Math.pow(value,2)}</p>)
+    // return "hello";
+  }
 const App = () => {
  //code here
-
+const [value, setValue] = React.useState(0);
   return (
     <div id="main">
       <ChildComponent /> 
-       <input id='input' onChange={} />
-    
-      <p id='output'>{}</p>
+       <input id='input' onChange={(e) => {setValue(e.target.value)}}/>
+      <ShowSquare value = {value}/>
     </div>
   )
 }
